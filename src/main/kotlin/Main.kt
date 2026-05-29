7a4d90be3d42806013481756bca61e146ade8e09
yashbokade2007@gmail.com <yashbokade2007@gmail.com>
2026-05-29 15:04:17 +0530

    working v1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,17 +10,19 @@ fun main(args: Array<String>) = runBlocking {
     val apiKey = "1f8Ibg4c7UovSyeK3VHfnM2WNyLI4beF"
     val a = File("D:\\Cursed\\cliHelper\\src\\main\\resources\\system.txt")
 //    print(a.readText())
+    var input: String? = ""
 
+    val restClient = MistralRestClient(apiKey)
     if (apiKey == "YOUR_API_KEY") {
         println("Please set the MISTRAL_API_KEY environment variable.")
         return@runBlocking
     }
-
-    val restClient = MistralRestClient(apiKey)
+    input = readLine()
     val promptMessages = listOf(
-        Message(role = "system", content = a.readText()) ,
-        Message(role = "user", content = args[1]+"\n only give the cmd output and no explanation of what is it doing or what will happen or in code block")
+        Message(role = "system", content = a.readText()),
+        Message(role = "user", content = input+"\n only give the cmd output and no explanation of what is it doing or what will happen or in code block | just plain text")
     )
+
 
     val req = ChatRequest(
         model = args[0],
@@ -34,8 +36,10 @@ fun main(args: Array<String>) = runBlocking {
         val textOutput = response.choices.firstOrNull()?.message?.content
 //        println("Response ID: ${response.id}")
 //        println("Tokens Used: ${response.usage.totalTokens}")
-        println(textOutput)
-        Runtime.getRuntime().exec("cmd /c $textOutput")
+        println(response)
+            Runtime.getRuntime().exec("cmd /c $textOutput")
+        promptMessages.plus(textOutput)
+
     } catch (e: Exception) {
         println("Synchronous execution error: ${e.message}\n")
     }
