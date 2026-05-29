d38cc33e027342170ebe8382a40a5ab26316ec45
yashbokade2007@gmail.com <yashbokade2007@gmail.com>
2026-05-29 17:39:56 +0530

    Blocked Checking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 package me.yashbokade
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.io.FileReader
 
+val AllowedCommands = listOf("ls", "dir", "cd", "mkdir", "echo", "clear", "exit")
+val BlockedCommands = listOf("rmdir","del")
 fun main(args: Array<String>) = runBlocking {
     val apiKey = "1f8Ibg4c7UovSyeK3VHfnM2WNyLI4beF"
     val a = File("D:\\Cursed\\cliHelper\\src\\main\\resources\\system.txt")
@@ -17,31 +16,44 @@ fun main(args: Array<String>) = runBlocking {
         println("Please set the MISTRAL_API_KEY environment variable.")
         return@runBlocking
     }
-    input = readLine()
-    val promptMessages = listOf(
-        Message(role = "system", content = a.readText()),
-        Message(role = "user", content = input+"\n only give the cmd output and no explanation of what is it doing or what will happen or in code block | just plain text")
-    )
+    while (input != "exit"){
+
+        input = readln()
+        val promptMessages = listOf(
+            Message(role = "system", content = a.readText()),
+            Message(role = "user", content = input+"\n only give the cmd output and no explanation of what is it doing or what will happen or in code block | just plain text")
+        )
 
 
-    val req = ChatRequest(
-        model = args[0],
-        messages = promptMessages,
-        temperature = 0.4,
+        val req = ChatRequest(
+            model = args[0],
+            messages = promptMessages,
+            temperature = 0.4,
 
-    )
+        )
 
-    try {
-        val response = restClient.complete(req)
-        val textOutput = response.choices.firstOrNull()?.message?.content
-//        println("Response ID: ${response.id}")
-//        println("Tokens Used: ${response.usage.totalTokens}")
-        println(response)
+        try {
+            val response = restClient.complete(req)
+            val textOutput = response.choices.firstOrNull()?.message?.content
+    //        println("Response ID: ${response.id}")
+    //        println("Tokens Used: ${response.usage.totalTokens}")
+            println(response)
+            val cmdToken: List<String>? = textOutput?.split(" ")
+
+            while (cmdToken?.size!=0){
+                if (BlockedCommands.contains(cmdToken?.get(0))){
+                    throw Exception("Blocked Command $textOutput")
+                }
+
+            }
+
+
             Runtime.getRuntime().exec("cmd /c $textOutput")
-        promptMessages.plus(textOutput)
+            promptMessages.plus(textOutput)
 
-    } catch (e: Exception) {
-        println("Synchronous execution error: ${e.message}\n")
+        } catch (e: Exception) {
+            println("Synchronous execution error: ${e.message}\n")
+        }
     }
 }
 
